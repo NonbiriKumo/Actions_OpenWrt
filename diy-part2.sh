a865a903e746873e5c1f96a8f2be3f873f492faa
NonbiriKumo <132285141+NonbiriKumo@users.noreply.github.com>
2025-11-27 23:23:57 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -19,4 +19,4 @@
 # Modify hostname
 #sed -i 's/OpenWrt/P3TERX-Router/g' package/base-files/files/bin/config_generate
 
-echo 'CONFIG_PACKAGE_luci-app-qmodem_next=y' >>.config
+echo 'CONFIG_PACKAGE_luci-app-qmodem=y' >>.config
